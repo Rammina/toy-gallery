24ac5250045e8d7bbf578dc20ed43c9bd9a3e57c
Rammina <altinaschwarzer@gmail.com>
2021-09-22 00:06:19 -0600

    Add pages, routes, and WindowContext for the application

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+import thunk from "redux-thunk";
+
+describe("App", () => {
+  const initialState = {};
+  const mockStore = configureStore([thunk]);
+  let store;
+
+  test("renders the application without errors", async () => {
+    store = mockStore(initialState);
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+    const appContainer = screen.getByTestId("app-outer-container");
+    expect(appContainer).toBeInTheDocument();
+  });
 });
